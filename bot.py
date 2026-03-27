e07157d2ecc891fcb237217410c1094400462054
sachin993986-maker <sachin993986@gmail.com>
2026-03-27 19:10:28 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+# /start command
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello 👋 Main tumhara Telegram bot hoon!")
+
+# normal messages
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+    await update.message.reply_text(f"Tumne bola: {user_message}")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    print("Bot chal raha hai...")
+    app.run_polling()
