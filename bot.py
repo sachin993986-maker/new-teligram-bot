8d0cc8446f28131b5630dee6c5522e0ea47f9a18
sachin993986-maker <sachin993986@gmail.com>
2026-03-27 19:18:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("8717123271:AAHQ845pbeY2rFUrxKUQPcLa_4BKe1VEf_I")
 
 # /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
